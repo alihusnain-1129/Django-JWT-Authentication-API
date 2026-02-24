d21109da99a3bce84fdc5485c0b2f2c7094efd16
alihusnain-1129 <alihusnain01129@gmail.com>
2026-02-24 17:51:36 +0500

    Django JWT Aunthentication API Project.

---
+++ auth_project/accounts/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "message": "You are authenticated!",
+            "user": request.user.username
+        })
